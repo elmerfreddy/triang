3b71ee9c5e39be0093b73ba3ab0faa76e8b148ec
Elmer Mendoza <emendoza@zenlabs.net>
2013-05-11 12:08:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triang::Application.config.secret_token = '650d2beac29c2fa3b56b0e3a9f5579023696e6bb28d2fa4b680cce6895506e7d0ee8153a57e53e0b1c77ba4681473cc0231c993d40f0ad891c04c2b305fc2573'
