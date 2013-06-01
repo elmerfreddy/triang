52d078859efec46e4d753f135b270096e281ea0d
Elmer Mendoza <defreddyelmer@gmail.com>
2013-06-01 00:09:36 -0400

    Get matches for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ActiveRecord::Base
   attr_accessible :foundation_date, :name, :category_id
 
   belongs_to :category
+
   has_many :matches
   has_many :team_vs, class_name: 'Team', through: :matches
 
@@ -9,5 +10,9 @@ class Team < ActiveRecord::Base
 
   def category_name
     category.name if category.present?
+  end
+
+  def matches_count
+    Match.where("team_id = ? OR team_vs_id = ?", id, id).count
   end
 end
